a31c02fc1eeac905d05c60d6e6564ce3bdb6a134
Maxim <therikomero@gmail.com>
2024-10-04 15:40:57 +0700

    add access control

--- src/widgets/AuthorizationWidget.js
+++ src/widgets/AuthorizationWidget.js
@@ -21,7 +21,9 @@ function AuthorizationWidget({checkToken}) {
             },
             {
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
                 }
             })
             .then(function (response) {
